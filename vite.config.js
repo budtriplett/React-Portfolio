633dd2d326db7156c0bea66cd0cd4138e1bac233
budtriplett <bud.triplett@yahoo.com>
2025-03-26 14:46:30 -0500

    package fixes again

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+  },
   server: {
-    port: 3000,
-    open: true
+    port: 3000,    
+    open: true,   
   }
 })
