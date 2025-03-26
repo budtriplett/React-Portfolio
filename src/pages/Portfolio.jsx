1e44cb1bf748ea38f02fc62f9d6d5504f6c7faed
budtriplett <bud.triplett@yahoo.com>
2025-03-26 13:36:58 -0500

    bug fixes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './portfolio.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap-icons/font/bootstrap-icons.css';
+//import './portfolio.css';
 import projects  from '../components/UI/projects';
 
 const Portfolio = () => {
